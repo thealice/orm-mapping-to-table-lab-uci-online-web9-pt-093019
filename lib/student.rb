e5e0aacbfe4bc7012be2f8e7e0c3f85068181483
Alice Freda <alicefreda@gmail.com>
2019-12-16 19:17:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,13 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
-      )
+      );
       SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students;"
     DB[:conn].execute(sql)
   end
   # Remember, you can access your database connection anywhere in this class
