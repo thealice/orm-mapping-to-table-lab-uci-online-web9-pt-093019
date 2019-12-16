02cc7233deeda6c7241ea9260f2bdfadce06ea98
Alice Freda <alicefreda@gmail.com>
2019-12-16 19:44:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(attributes)
     @name = name
     @grade = grade
     @id = id
